2fa84ea5df4b8ddeb60dc2ed018acfa30dd0635b
ShaNi <sharkshark.ni@gmail.com>
2021-09-30 23:15:33 +0100

    basic framework of the flask app

---
+++ FlaskTemplate/flaskApp/routes.py
@@ -0,0 +1,18 @@
+# Flask
+from flask import render_template, redirect, url_for, request, session, flash, send_from_directory
+from sqlalchemy import or_, and_
+from werkzeug.security import generate_password_hash, check_password_hash
+from flaskApp import app
+
+
+@app.route('/', methods=['GET', 'POST'])
+# dashboard index page
+@app.route('/index', methods=['GET', 'POST'])
+def index():
+    return render_template('index.html')
+
+
+# testing page
+@app.route('/blank', methods=['GET', 'POST'])
+def blank():
+    return render_template('blank.html')
